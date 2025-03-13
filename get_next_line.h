5deb4e1de8aa8bdb3de658416c911249afa6b08f
Leonor Martinez Mesas <leonmart@c3r5s4.42malaga.com>
2025-03-13 14:07:58 +0100

    auxiliar functions

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: leonmart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 12:38:49 by leonmart          #+#    #+#             */
-/*   Updated: 2025/03/12 13:12:16 by leonmart         ###   ########.fr       */
+/*   Updated: 2025/03/13 13:07:08 by leonmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,54 @@
 # include <fcntl.h>
 # include <stdlib.h>
 # include <unistd.h>
-
-void	ft_putchar(char c);
-int     ft_strlen(const char    *str);
+/**
+ * @brief ft_strlen(str) return the length of the given string
+ * 
+ * @param str If "str" doesn't exist (is 0 or NULL) the function return NULL.
+ * @return ** unsigned int to can used in other functions
+ */
+int		ft_strlen(const char *str);
+/**
+ * @brief ft_strlcpy(dst,src,dstsize) if "dstsize" is not 0,
+ * "ft_strlcpy()" copies up to (dstsize - 1)
+ * characters from the string src to dst, adding a NUL-terminating.
+ * 
+ * @param dst It is necesarry declare it with malloc before passing 
+ * it to the function.
+ * @param src It is necesarry declare it with malloc before passing 
+ * it to the function.
+ * @param dstsize if "dstsize = 0" the fun
+ * @return ** unsigned int,  always the length of src.
+ */
+int		ft_strlcpy(char *dst, const char *src, unsigned int dstsize);
+/**
+ * @brief ft_calloc(count,size) allocate memory for count * size bytes,
+ * and full that memory with 0
+ * 
+ * @param  "count" number of positions, "size" bytes for every position
+ * @return *void a pointer to the first byte of the allocate memory
+ */
+void	*ft_calloc(size_t count, size_t size);
+/**
+ * @brief ft_strjoin(*s1,*s2) allocates memory for a new string with length
+ * (length s1 + length s2),and copy the contents of s1 and s2 into it.
+ *
+ * @param s1 if "s1" doesn't exist (is 0 or NULL)
+ *  the function cause a :!! segmenttion fault !!.
+ * @param s2 if "s2" doesn't exist (is 0 or NULL)
+ *  the function cause a :!! segmenttion fault !!.
+ * @return char*
+ */
+char	*ft_strjoin(char const *s1, char const *s2);
+/**
+ * @brief The ft_strchr(*s,c) function locates the FIRST occurrence of "c"
+ * in the string "s";
+ *
+ * @param s a string of characters.if "s" doesn't exist (is 0 or NULL)
+ *  the function cause a: !! segmenttion fault !!.
+ * @param c int the ascii integer value of the char to locate.
+ * @return a char* pointer to the first (char)c int *s,
+ * if (char)c is not present in *s, return 0.
+ */
+char	*ft_strchr(const char *s, int c);
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,16 +6,11 @@
 /*   By: leonmart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 12:41:20 by leonmart          #+#    #+#             */
-/*   Updated: 2025/03/12 13:41:46 by leonmart         ###   ########.fr       */
+/*   Updated: 2025/03/13 12:52:21 by leonmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
 
 int	ft_strlen(const char	*str)
 {
@@ -29,21 +24,21 @@ int	ft_strlen(const char	*str)
 	return (count);
 }
 
-int	ft_strlcpy(char *dest, const char *src, unsigned int destsize)
+int	ft_strlcpy(char *dst, const char *src, unsigned int dstsize)
 {
 	int	index;
 	int	src_len;
 
 	index = 0;
 	src_len = ft_strlen(src);
-	if (destsize != 0)
+	if (dstsize != 0)
 	{
-		while (src[index] != 0 && index < (destsize - 1))
+		while (src[index] != 0 && index < (dstsize - 1))
 		{
-			dest[index] = src[index];
+			dst[index] = src[index];
 			index++;
 		}
-		dest[index] = '\0';
+		dst[index] = '\0';
 	}
 	return (src_len);
 }
@@ -54,13 +49,58 @@ void	*ft_calloc(size_t count, size_t size)
 	size_t	index;
 
 	index = 0;
-	ptr = malloc(count * size);
+	ptr = (char *) malloc(count * size);
 	if (!ptr)
-	return (NULL);		
+		return (NULL);
 	while (index < (count * size))
-	{	
+	{
 		ptr[index] = 0;
 		index++;
 	}
 	return ((void *)ptr);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int		s1_index;
+	int		s2_index;
+	int		str_joined_index;
+
+	s1_index = 0;
+	s2_index = 0;
+	str_joined_index = 0;
+	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!str || (!s1 && !s2))
+		return (NULL);
+	while (s1[s1_index] != '\0')
+	{
+		str[str_joined_index] = s1[s1_index];
+		str_joined_index++;
+		s1_index++;
+	}
+	while (s2[s2_index] != '\0')
+	{
+		str[str_joined_index] = s2[s2_index];
+		str_joined_index++;
+		s2_index++;
+	}
+	str[str_joined_index] = '\0';
+	return (str);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	index;
+
+	index = 0;
+	while (s[index] != '\0')
+	{
+		if (s[index] == (char)c)
+			return ((char *)(s + index));
+		index++;
+	}
+	if (s[index] == (char)c)
+		return ((char *)(s + index));
+	return (0);
 }
